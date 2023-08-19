bab064517ef50c51fd2784a22ba17cff8e41ea38
Berwyn Tan <berwyntan@gmail.com>
2023-08-19 15:35:32 +0800

    setup vitest

---
+++ src/testing/DataLoadAndMatchForm.test.tsx
@@ -0,0 +1,40 @@
+import '@testing-library/jest-dom';
+import { describe, expect, it } from "vitest";
+import { screen, waitFor } from "@testing-library/react";
+import { renderWithBrowserRouter } from "./setup/renderFunctions";
+import DataLoadAndMatchForm from "@/components/DataLoadAndMatchForm";
+
+describe("Render test", () => {
+  it("Render test", () => {
+    // renderWithBrowserRouter(<DataLoadAndMatchForm />);
+
+    // const title = screen.getByText(/Step 1/);
+    // expect(title).toBeInTheDocument();
+  });
+  // it('renders mock data', async () => {
+
+  //     renderWithQueryClientMemoryRouter(
+  //       <AdminCategory />,
+  //       '/api/product/cat/count/12345',
+  //       '/api/product/cat/count/:id'
+  //     )
+
+  //     await waitFor(() => {
+  //       const product = screen.getByText(/Creator Expert 10280 Flower Bouquet/)
+  //       expect(product).toBeInTheDocument()
+  //     })
+  // })
+  // it('server error 400', async () => {
+
+  //     renderWithQueryClientMemoryRouter(
+  //       <AdminCategory />,
+  //       '/api/product/cat/count/sadcase',
+  //       '/api/product/cat/count/:id'
+  //     )
+
+  //     await waitFor(() => {
+  //       const message = screen.getByText(/Server error/)
+  //       expect(message).toBeInTheDocument()
+  //     })
+  // })
+});
---
+++ src/testing/setup/setup.ts
@@ -0,0 +1,19 @@
+import { expect, afterEach, afterAll, beforeAll } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import matchers from '@testing-library/jest-dom/matchers';
+import {server} from './server.js'
+
+// extends Vitest's expect method with methods from react-testing-library
+expect.extend(matchers);
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
+
+
+
+// runs a cleanup after each test case (e.g. clearing jsdom)
+afterEach(() => {
+  cleanup();
+  server.resetHandlers();
+});
+
+
+afterAll(() => server.close())
